8fedc698145a3076e11f53dc8a9851fb2258cf2a
Patrick Mwangi <frashid274@gmail.com>
2025-09-12 03:45:47 +0300

    add Wishlist component to display wishlist items

---
+++ frontend/src/pages/Wishlist.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Wishlist = () => {
+    return (
+        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
+            <h1 className="text-3xl font-bold mb-8">My Wishlist</h1>
+            <p>Wishlist items will be displayed here</p>
+        </div>
+    );
+};
+
+export default Wishlist;
